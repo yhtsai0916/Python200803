e483d806ae5d35856e5417460ffc9463137c13ef
yhtsai0916 <69131789+yhtsai0916@users.noreply.github.com>
2020-08-03 11:17:02 +0800

    Add files via upload

---
+++ Day1-1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:06:12 2020
+
+@author: USER
+"""
+
+print(" I'm Eric \nHe is\"cool\"")
+
+
+
+     
---
+++ Day1-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:28:52 2020
+
+@author: USER
+"""
+
+name=input("enter your name:\n")
+print(name+",Hi")
