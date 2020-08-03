e861e3aa299e434ed77416dcad42c2846516128e
yhtsai0916 <69131789+yhtsai0916@users.noreply.github.com>
2020-08-03 11:57:50 +0800

    Add files via upload

---
+++ Day1-3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 11:23:50 2020
+
+@author: USER
+"""
+
+r=input("輸入圓半徑:")
+ans1=float(r)*2*3.14
+ans2=float(r)*float(r)*3.14
+print("周長:",ans1)
+print("面積:",ans2)
